cd5d61ef8a4ae5aa38a242f7cc7167b982a789bf
Netanel Hadas <netanel.hadas@gmail.com>
2017-06-29 16:10:08 +0300

    checking the first method in app.js res.sendstatus is giving me an error need to ask ben to try and run it and see if it gives him the same error.

--- app.js
+++ app.js
@@ -21,48 +21,76 @@ app.use(cookieParser());
 
 // START THE SERVER
 // =============================================================================
-var port = process.env.PORT || 8080;        // set our port
+//var port = process.env.PORT || 8080;        // set our port
+var port = 8080;
 app.listen(port);
 console.log('Magic happens on port ' + port);
 
-var uidCounter = 1;
-var logedInUsers = {};
-var logedInUsersTag = {};
 
-
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
-// ROUTES FOR OUR API
-// =============================================================================
-             
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-app.post('/register/:username/:password',function(req,res,next){
-    
-    console.log("body",req.body);
-    
-    var uid = logedInUsers[req.params.user];
-    if(uid){
-        res.cookie('uid', uid);
-    }else{
-        uidCounter++;
-        logedInUsers[req.params.user] = uidCounter;
-        logedInUsersTag[uidCounter] = req.params.user;
-        res.cookie('uid', uidCounter);
+//All events are going to be stored in here
+var events = [
+    {
+        name: "Herzelia, basketball game, need 3 more players",
+        time: "Thu, 21 Jun 2017 19:00:00 GMT",
+        location: "Herzelia, park",
+        activity: "basketball",
+        img: "https://www.courtsoftheworld.com/upload/courts/248/0/COTW_Tupper-Park_1244670982.jpg",
+        id: "0"
+    },
+    {
+        name: "Herzelia, video gaming, need 1 more players",
+        time: "Sat, 1 July 2017 20:30:00 GMT",
+        location: "Herzelia, ezel street 28",
+        activity: "playing dota 2",
+        img: "http://cdn.dota2.com/apps/dota2/images/blogfiles/wh_blog.jpg",
+        id: "1"
     }
-    res.json({msg: "OK", secretKey: 23});
+];
 
+// adding events by users of the app
+function addEvent(name,time, location, activity, img) {
+    this.name = name;
+	this.time = time;
+    this.location = location;
+    this.activity = activity;
+    this.img = img;
+}
 
+// array to hold our users
+var users = [
+    {
+        uid: "1",
+        username : "admin",
+        password: "1234"
+    }
+];
+
+  
+// add a user and password to the user-list
+app.post("/register/:username/:password",function(req,res,next){
+    let username = req.params.username;
+    let password = req.params.password;
+    console.log("username = " + username);
+    console.log("password = " + password);
+    let userExist = false;
+	
+	// checks if the user already exists
+	// or if its a new user we need to add
+    for(i = 0; i < users.length; i++){
+        if (users[i].username === username){
+            userExist = true;
+            break;
+        }
+    }
+    if (userExist){
+        console.log("user already exists");
+        res.sendStatus(500);
+    } else {
+        console.log("Registration successful");
+        users.push({username: username, password: password});
+        res.sendStatus(200);
+    }
 });
 
 
-module.exports = app;
+
